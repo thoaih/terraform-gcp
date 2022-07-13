435bc597992e9a726f068c517327908354bcbd21
ila-bi <109213428+ila-bi@users.noreply.github.com>
2022-07-13 17:09:37 +0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+  default = "ila-collab"
+}
+
+variable "credentials_file" {
+  default = "/Users/thoai.ho/Downloads/ila-collab-terraform.json"
+}
+
+variable "region" {
+  default = "asia-southeast1"
+}
+
+variable "zone" {
+  default = "asia-southeast1-a"
+}
+
+variable "network" {
+  default = "airflow-network"
+}
