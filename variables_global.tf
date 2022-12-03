a6b6021be5e59f2b7d303c9f007d98af95d11d78
Thoai Ho <thoaihn@ahamove.com>
2022-12-03 15:10:54 +0700

    modified:   main.tf 	modified:   variables_global.tf

--- variables_global.tf
+++ variables_global.tf
@@ -2,7 +2,7 @@
 variable "project" {
   description = "Project ID on Google Cloud Platform"
   type        = string
-  default     = "uit-cloud-computing"
+  default     = ""
 }
 
 variable "GOOGLE_CREDENTIALS" {
