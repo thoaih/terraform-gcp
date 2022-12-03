f0d06159c5dd59a5539717e0a112399d2d3bbe74
Thoai Ho <thoaihn@ahamove.com>
2022-12-03 23:11:24 +0700

    modified:   variables_sql-database.tf

--- variables_sql-database.tf
+++ variables_sql-database.tf
@@ -21,7 +21,7 @@ variable "db_machine_type" {
 variable "db_disk_size_gb" {
   description = "The size of data disk, in GB"
   type        = number
-  default     = 10
+  default     = 12
 }
 
 variable "db_disk_type" {
